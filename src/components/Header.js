0c085acdee6ec126e15f95310720cc312f3e427e
shubham <skushwah106@gmail.com>
2024-12-18 23:44:19 +0530

    sign in page added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,3 @@
-// import React from "react";
-// import "../stylesheets/Header.css";
-// import { FaAngleDown } from "react-icons/fa6";
-// import { Link } from "react-router-dom";
-
-// const Header = () => {
-//   return (
-//     <header>
-//       <nav>
-//         <ul className="nav-list">
-//           <li>
-//             <Link to="/home">Home</Link>
-//           </li>
-//           <li>
-//             <Link to="/aboutus">About Us</Link>
-//           </li>
-//           <li>
-//             <Link to="/pricing">Pricing</Link>
-//           </li>
-//           <li>
-//             <Link to="/contactus">Contact Us</Link>
-//           </li>
-
-//           <div className="nav-btn">
-//             <Link to="/signin" className="login-btn">Sign In</Link>
-//             <Link to="/signup" className="signup-btn">Sign Up</Link>
-//           </div>
-
-//           <li>
-//             <div className="dropdown">
-//               <Link to="/" className="head-dropdown">
-//                 For Employee <FaAngleDown />
-//               </Link>
-//               <div className="dropdown-content">
-//                 <Link to="/employee/dashboard">Candidate</Link>
-//                 <Link to="/employee/profile">Employee</Link>
-//               </div>
-//             </div>
-//           </li>
-//         </ul>
-//       </nav>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
-
 import React from "react";
 import "../stylesheets/Header.css";
 import { FaAngleDown } from "react-icons/fa6";
@@ -56,7 +8,6 @@ const Header = () => {
     <header>
       <nav>
         <ul className="nav-list">
-          {/* Navigation Links */}
           <li>
             <Link to="/home">Home</Link>
           </li>
@@ -69,25 +20,25 @@ const Header = () => {
           <li>
             <Link to="/contactus">Contact Us</Link>
           </li>
-          </ul>
-          {/* Authentication Buttons */}
-          <div className="nav-btn">
-            <Link to="/signin" className="sign-btn">Sign In</Link>
-            <Link to="/signup" className="signup-btn">Sign Up</Link>
-          </div>
+        </ul>
+        <div className="nav-btn">
+          <Link to="/signin" className="sign-btn">
+            Sign In
+          </Link>
+          <Link to="/signup" className="signup-btn">
+            Sign Up
+          </Link>
+        </div>
 
-          {/* Dropdown for "For Employee" */}
-          <li className="dropdown">
-            <Link to="/" className="head-dropdown">
-              For Employee <FaAngleDown />
-            </Link>
-            {/* Dropdown Content */}
-            <div className="dropdown-content">
-              <Link to="/candidate">Candidate</Link>
-              <Link to="/employee">Employee</Link>
-            </div>
-          </li>
-        
+        <li className="dropdown">
+          <Link to="/" className="head-dropdown">
+            For Employee <FaAngleDown />
+          </Link>
+          <div className="dropdown-content">
+            <Link to="/candidate">Candidate</Link>
+            <Link to="/employee">Employee</Link>
+          </div>
+        </li>
       </nav>
     </header>
   );
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,51 +1,123 @@
-import React from "react";
+import React, { useState } from "react";
 import "../stylesheets/SignIn.css";
 import signInEmp from "../images/signInEmp.png";
-import SignUp from "../components/SignUp";
 import { FaHandsClapping } from "react-icons/fa6";
+import SignUp from "./SignUp";
 
 const SignIn = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
   return (
     <>
       <div className="signinBox">
-        <div className="signIn">
-          <div className="signin-left">
-            <img src={signInEmp} alt="" />
-            <h1>Let Job Find You</h1>
-            <p>
-              you will never know everything <br />
-              But you will Konw more.
-            </p>
+        <div className="signin-container">
+          <div className="left-box">
+            {isLogin ? (
+              <>
+                <div className="signin-left">
+                  <img src={signInEmp} alt="" />
+                  <h3>Let Job Find You</h3>
+                  <p>
+                    you will never know everything <br />
+                    But you will Konw more
+                  </p>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="signin-left">
+                  <img src={signInEmp} alt="" />
+                  <h3>Find Immediate Joiners for You</h3>
+                  <p>
+                    you will never know everything <br />
+                    But you will Konw more
+                  </p>
+                </div>
+              </>
+            )}
           </div>
-          <div className="signin-right">
-            <div className="signin-right-box">
-              <FaHandsClapping color="yellow" />
-              <h2>Welcome Back!</h2>
-              <p>Please login to access your account.</p>
-              <div className="signIn-swithBtn">
-                <button className="empBtn" >Employee</button>Ï
-                <button className="jobSeekerBtn">Job Seeker</button>
+          <div className="right-form">
+            <div className="right-form-head">
+              <FaHandsClapping className="hello-icon" />
+              <h2>WelCome back!</h2>
+              <p>Please login to access your account</p>
+            </div>
+            <div className="signInRight">
+              <div className="signInForm">
+                <div className="signIn-toggle">
+                  <button
+                    className={isLogin ? "active signBtn" : ""}
+                    onClick={() => setIsLogin(true)}
+                  >
+                    Employee
+                  </button>
+                  <button
+                    className={!isLogin ? "active signBtn" : ""}
+                    onClick={() => setIsLogin(false)}
+                  >
+                    Job seeker
+                  </button>
+                </div>
+                {isLogin ? (
+                  <>
+                    <div className="form">
+                      <div className="form-emp">
+                        <div className="form-group">
+                          <label htmlFor="mobile">Mobile Number</label>
+                          <div className="input-otp">
+                            <input
+                              type="number"
+                              id="mobile"
+                              placeholder="Enter your mobile number"
+                            />
+                            <button>Send otp</button>
+                          </div>
+                        </div>
+                        <div className="form-group">
+                          <label htmlFor="mobile">OTP</label>
+                          <input
+                            type="number"
+                            id="otp"
+                            placeholder="Enter OTP"
+                          />
+                        </div>
+                      </div>
+                      <div className="login-btn">
+                        <button>Log In</button>
+                        <p>
+                          Don't have an account <a href="" onClick={() => setIsLogin(<SignUp />)}>Register now</a>
+                        </p>
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className="form">
+                      <div className="form-group">
+                        <label htmlFor="mobile">Mobile Number</label>
+                        <div className="input-otp">
+                          <input
+                            type="number"
+                            id="mobile"
+                            placeholder="Enter your mobile number"
+                          />
+                          <button>Send otp</button>
+                        </div>{" "}
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="mobile">OTP</label>
+                        <input type="number" id="otp" placeholder="Enter OTP" />
+                      </div>
+                      <div className="login-btn">
+                        <button>Log In</button>
+                        <p>
+                          Don't have an account <a href="#">Register now</a>
+                        </p>
+                      </div>
+                    </div>
+                  </>
+                )}
               </div>
-              <form>
-                <div className="form-group">
-                  <label htmlFor="mobile">Mobile Number</label>
-                  <input
-                    type="text"
-                    id="mobile"
-                    placeholder="Enter your mobile number"
-                  />
-                </div>
-                <button>Send OTP</button>
-
-                <div className="form-group">
-                  <label htmlFor="otp">OTP</label>
-                  <input type="text" id="otp" placeholder="Enter OTP" />
-                </div>
-              </form>
-              <button>login</button>
-              <p>
-                Don't have and account? <a href={SignUp}>Register here</a>
-              </p>
             </div>
           </div>
         </div>
