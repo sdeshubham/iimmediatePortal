5b6d11dcc863953f9e0088883834726e2661a18f
shubham <skushwah106@gmail.com>
2025-01-06 20:18:54 +0530

    responbsive and APibindding started

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,101 @@
-import React from "react";
+// import React, { useState } from "react";
+// import "../stylesheets/Header.css";
+// import { FaAngleDown, FaBars, FaTimes } from "react-icons/fa";
+// import { Link } from "react-router-dom";
+
+// const Header = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <header>
+//       <nav>
+//         <ul className={`nav-list ${isOpen ? "open" : ""}`}>
+//           <li>
+//             <Link to="/home" onClick={toggleMenu}>
+//               Home
+//             </Link>
+//           </li>
+//           <li>
+//             <Link to="/aboutus" onClick={toggleMenu}>
+//               About Us
+//             </Link>
+//           </li>
+//           <li>
+//             <Link to="/contactus" onClick={toggleMenu}>
+//               Contact Us
+//             </Link>
+//           </li>
+//         </ul>
+
+//         <div className="nav-btn">
+//           <Link to="/signin" className="sign-btn" onClick={toggleMenu}>
+//             Sign In
+//           </Link>
+//           <Link to="/signup" className="signup-btn" onClick={toggleMenu}>
+//             Sign Up
+//           </Link>
+//         </div>
+
+//         <li className="dropdown">
+//           <Link to="/" className="head-dropdown">
+//             Select For
+//             <FaAngleDown />
+//           </Link>
+//           <div className="dropdown-content">
+//             <Link to="/candidate" onClick={toggleMenu}>
+//               Employee
+//             </Link>
+//             <Link to="/employer" onClick={toggleMenu}>
+//               Employer
+//             </Link>
+//           </div>
+//         </li>
+
+//         <div className="hamburger" onClick={toggleMenu}>
+//           {isOpen ? <FaTimes /> : <FaBars />}
+//         </div>
+//       </nav>
+//     </header>
+//   );
+// };
+
+// export default Header;
+
+
+import React, { useState } from "react";
 import "../stylesheets/Header.css";
-import { FaAngleDown } from "react-icons/fa6";
+import { FaBars } from "react-icons/fa"; // Hamburger Icon
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false); // State for toggling menu
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header>
       <nav>
-        <ul className="nav-list">
+        <div className="hamburger" onClick={toggleMenu}>
+          <FaBars /> {/* Hamburger icon */}
+        </div>
+        <ul className={`nav-list ${menuOpen ? "open" : ""}`}>
           <li>
             <Link to="/home">Home</Link>
           </li>
           <li>
             <Link to="/aboutus">About Us</Link>
           </li>
-          {/* <li>
-            <Link to="/pricing">Pricing</Link>
-          </li> */}
           <li>
             <Link to="/contactus">Contact Us</Link>
           </li>
         </ul>
-        <div className="nav-btn">
+        <div className={`nav-btn ${menuOpen ? "open" : ""}`}>
           <Link to="/signin" className="sign-btn">
             Sign In
           </Link>
@@ -29,17 +103,6 @@ const Header = () => {
             Sign Up
           </Link>
         </div>
-
-        <li className="dropdown">
-          <Link to="/" className="head-dropdown">
-            Select For
-            <FaAngleDown />
-          </Link>
-          <div className="dropdown-content">
-            <Link to="/candidate">Employee</Link>
-            <Link to="/employer">Employer</Link>
-          </div>
-        </li>
       </nav>
     </header>
   );
